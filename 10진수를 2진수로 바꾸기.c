eb0e0b957d9d4ffd816a5dfef6fed106672967a8
ho0010 <129824629+ho0010@users.noreply.github.com>
2023-04-06 10:35:10 +0900

    Add files via upload

---
+++ 10진수를 2진수로 바꾸기.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{	// 어려워요,,,
+	unsigned int num;
+	printf("십진수: ");
+	scanf("%u", &num);
+
+	unsigned int mask = 1 << 7; //mask=10000000
+	printf("이진수: ");
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	mask = mask >> 1;
+	((num & mask) == 0) ? printf("0") : printf("1");
+	printf("\n");
+}
